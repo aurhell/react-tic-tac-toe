d64e7ac4266d649675f70de64580ea3ca4e41074
Aurélien Girault <me@agirault.com>
2019-07-23 15:07:22 +0200

    adding some minor style + moving to files

---
+++ src/Square.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Square(props) {
+    return (
+      <button 
+          className={ "square " + getColor(props.value)}
+          onClick={props.onClick}
+        >
+          {props.value}
+      </button>
+    )
+  }
+
+  function getColor(value) {
+    if (value === 'X') {
+      return 'square-red';
+    }
+    if (value === '0') {
+      return 'square-green';
+    } 
+    return;
+  }
+
+export default Square;
